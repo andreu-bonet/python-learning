39037e34b516cead1a48c471a1487bcbb5995513
Andreu Bonet <andreu.bonet2@gmail.com>
2024-06-19 15:30:48 +0200

    Update

---
+++ 2d_arrays.py
@@ -0,0 +1,13 @@
+rows = 3
+cols = 3
+value = 7
+matrix = [[[] for i in range(rows)] for i in range(cols)]
+
+print(matrix)
+
+string_1 = "ABCDER"
+string_2 = "LAGEWR"
+
+grid = [[0 for col in range(len(string_1) + 1)] for row in range(len(string_2) + 1)]
+
+print(grid)
